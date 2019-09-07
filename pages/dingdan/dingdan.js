0cefb49a53614a04772d58a7f6cbccabe0414567
lch <xiangyanghua929@163.com>
2019-09-07 09:31:51 +0800

    订单更新
    9月7
    9:31

--- pages/dingdan/dingdan.js
+++ pages/dingdan/dingdan.js
@@ -6,7 +6,7 @@ Page({
    */
   data: {
     currtab: 0,
-    swipertab: [{ name: '已完成', index: 0 }, { name: '待付款', index: 1 }, { name: '已付款', index: 2 },{ name:"配送中",index:3},{name:"已完成",index:4}],
+    swipertab: [{ name: '全 部', index: 0 }, { name: '待付款', index: 1 }, { name: '已付款', index: 2 },{ name:"配送中",index:3},{name:"已完成",index:4}],
   },
  
   /**
@@ -67,12 +67,17 @@ Page({
       case 2:
         that.lostShow()
         break
-      
+      case 3:
+        that.pszShow()
+         break  
+      case 4:
+        that.ywcShow()
+         break
     }
   },
   alreadyShow: function(){
     this.setData({
-      alreadyOrder: [{ name: "635219506", state: "交易成功", time: "2018-09-30 14:00-16:00", status: "已结束", url: "", money: "132" }, { name: "532521652", state: "交易成功", time: "2018-10-12 18:00-20:00", status: "未开始", url: "", money: "205" }]
+      alreadyOrder: [{ name: "635219506", state: "待付款", time: "2018-09-30 14:00-16:00", status: "已结束", url: "", money: "132" }, { name: "532521652", state: "配送中", time: "2018-10-12 18:00-20:00", status: "未开始", url: "", money: "205" }]
     })
   },
  
@@ -84,9 +89,18 @@ Page({
  
   lostShow: function () {
     this.setData({
-      lostOrder: [{ name: "931717287", state: "已取消", time: "2018-10-4 10:00-12:00", status: "未开始", url: "", money: "122" }],
+      lostOrder: [{ name: "931717287", state: "已付款", time: "2018-10-4 10:00-12:00", status: "未开始", url: "", money: "122" }],
     })
   },
- 
+ pszShow: function(){
+    this.setData({
+      pszOrder: [{ name: "5526265136", state: "配送中", time: "2018-09-30 14:00-16:00", status: "已结束", url: "", money: "132" }]
+    })
+  },
+ ywcShow: function(){
+    this.setData({
+      ywcOrder: [{ name: "555555554", state: "已完成", time: "2018-09-30 14:00-16:00", status: "已结束", url: "", money: "132" }]
+    })
+  },
   
 })
