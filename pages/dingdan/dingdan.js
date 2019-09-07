18d44db472cc7a81e2da61c15d497fe1ba086b32
lch <xiangyanghua929@163.com>
2019-09-07 11:47:42 +0800

    添加订单详情
    订单详情未完成

--- pages/dingdan/dingdan.js
+++ pages/dingdan/dingdan.js
@@ -1,33 +1,54 @@
 // pages/order/order.js
 Page({
- 
+
   /**
    * 页面的初始数据
    */
   data: {
     currtab: 0,
-    swipertab: [{ name: '全 部', index: 0 }, { name: '待付款', index: 1 }, { name: '已付款', index: 2 },{ name:"配送中",index:3},{name:"已完成",index:4}],
+    swipertab: [{
+      name: '全 部',
+      index: 0
+    }, {
+      name: '待付款',
+      index: 1
+    }, {
+      name: '已付款',
+      index: 2
+    }, {
+      name: "配送中",
+      index: 3
+    }, {
+      name: "已完成",
+      index: 4
+    }],
   },
- 
+
+xiangqing:function(e){
+console.log(12121331);
+wx.navigateTo({
+  url: '/pages/dingdan/wy/wy'
+})
+},
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
- 
+  onLoad: function(options) {
+
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
     // 页面渲染完成
     this.getDeviceInfo()
     this.orderShow()
   },
- 
-  getDeviceInfo: function () {
+
+  getDeviceInfo: function() {
     let that = this
     wx.getSystemInfo({
-      success: function (res) {
+      success: function(res) {
         that.setData({
           deviceW: res.windowWidth,
           deviceH: res.windowHeight
@@ -35,11 +56,11 @@ Page({
       }
     })
   },
- 
+
   /**
-  * @Explain：选项卡点击切换
-  */
-  tabSwitch: function (e) {
+   * @Explain：选项卡点击切换
+   */
+  tabSwitch: function(e) {
     var that = this
     if (this.data.currtab === e.target.dataset.current) {
       return false
@@ -49,13 +70,15 @@ Page({
       })
     }
   },
- 
-  tabChange: function (e) {
-    this.setData({ currtab: e.detail.current })
+
+  tabChange: function(e) {
+    this.setData({
+      currtab: e.detail.current
+    })
     this.orderShow()
   },
- 
-  orderShow: function () {
+
+  orderShow: function() {
     let that = this
     switch (this.data.currtab) {
       case 0:
@@ -69,38 +92,83 @@ Page({
         break
       case 3:
         that.pszShow()
-         break  
+        break
       case 4:
         that.ywcShow()
-         break
+        break
     }
   },
-  alreadyShow: function(){
+  alreadyShow: function() {
     this.setData({
-      alreadyOrder: [{ name: "635219506", state: "待付款", time: "2018-09-30 14:00-16:00", status: "已结束", url: "../../images/美食1.jpeg", money: "132" }, { name: "532521652", state: "配送中", time: "2018-10-12 18:00-20:00", status: "未开始", url: "../../images/美食2.jpeg", money: "205" }]
+      alreadyOrder: [{
+        name: "635219506",
+        state: "待付款",
+        time: "2018-09-30 14:00-16:00",
+        status: "已结束",
+        url: "../../images/美食1.jpeg",
+        money: "132"
+      }, {
+        name: "532521652",
+        state: "配送中",
+        time: "2018-10-12 18:00-20:00",
+        status: "未开始",
+        url: "../../images/美食2.jpeg",
+        money: "205"
+      }]
     })
   },
- 
-  waitPayShow:function(){
+
+  waitPayShow: function() {
     this.setData({
-      waitPayOrder: [{ name: "100454577", state: "待付款", time: "2018-10-14 14:00-16:00", status: "未开始", url: "../../images/美食3.jpeg", money: "186" }],
+      waitPayOrder: [{
+        name: "100454577",
+        state: "待付款",
+        time: "2018-10-14 14:00-16:00",
+        status: "未开始",
+        url: "../../images/美食3.jpeg",
+        money: "186"
+      }],
     })
   },
- 
-  lostShow: function () {
+
+  lostShow: function() {
     this.setData({
-      lostOrder: [{ name: "931717287", state: "已付款", time: "2018-10-4 10:00-12:00", status: "未开始", url: "../../images/美食1.jpeg", money: "122" }],
+      lostOrder: [{
+        name: "931717287",
+        state: "已付款",
+        time: "2018-10-4 10:00-12:00",
+        status: "未开始",
+        url: "../../images/美食1.jpeg",
+        money: "122"
+      }],
     })
   },
- pszShow: function(){
+  pszShow: function() {
     this.setData({
-      pszOrder: [{ name: "5526265136", state: "配送中", time: "2018-09-30 14:00-16:00", status: "已结束", url: "../../images/美食2.jpeg", money: "132" }]
+      pszOrder: [{
+        name: "5526265136",
+        state: "配送中",
+        time: "2018-09-30 14:00-16:00",
+        status: "已结束",
+        url: "../../images/美食2.jpeg",
+        money: "132"
+      }]
     })
   },
- ywcShow: function(){
+  ywcShow: function() {
     this.setData({
-      ywcOrder: [{ name: "555555554", state: "已完成", time: "2018-09-30 14:00-16:00", status: "已结束", url: "../../images/美食1.jpeg", money: "132" }]
+      ywcOrder: [{
+        name: "555555554",
+        state: "已完成",
+        time: "2018-09-30 14:00-16:00",
+        status: "已结束",
+        url: "../../images/美食1.jpeg",
+        money: "132"
+      }]
     })
   },
-  
+
+
+
+
 })
